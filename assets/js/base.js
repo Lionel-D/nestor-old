973dd3f8078227618e801f9a6a039c81375caf59
Lionel-D <hello@lionel-d.com>
2019-08-10 17:56:36 +0200

    add basic structure for assets

---
+++ assets/js/base.js
@@ -0,0 +1,14 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Any CSS you require will output into a single CSS file
+require('../scss/base.scss');
+
+// Any JS you require will output into a single JS file
+const $ = require('jquery');
+require('popper.js');
+require('bootstrap');
