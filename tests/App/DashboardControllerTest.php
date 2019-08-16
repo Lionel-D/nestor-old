c3b252982590b04be42917bca6f2de7bfaaf3333
Lionel-D <hello@lionel-d.com>
2019-08-16 16:26:53 +0200

    refactor test to add authentication

--- tests/App/DashboardControllerTest.php
+++ tests/App/DashboardControllerTest.php
@@ -2,7 +2,13 @@
 
 namespace App\Tests\App;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 
 /**
  * Class DashboardControllerTest
@@ -11,13 +17,48 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DashboardControllerTest extends WebTestCase
 {
+    /**
+     * @var KernelBrowser
+     */
+    private $client;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->entityManager = $this->client->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function testDashboardDisplay()
     {
-        $client = static::createClient();
+        $this->logIn();
 
-        $client->request('GET', '/app/dashboard');
+        $this->client->request('GET', '/app/dashboard');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Hello DashboardController!');
+    }
+
+    private function logIn()
+    {
+        /** @var UserInterface $user */
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => 'hello@lionel-d.com']);
+        $session = $this->client->getContainer()->get('session');
+        $firewall = 'main';
+
+        $token = new PostAuthenticationGuardToken($user, '_security_'.$firewall, ['ROLE_ADMIN']);
+
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+
+        $this->client->getCookieJar()->set($cookie);
     }
 }
