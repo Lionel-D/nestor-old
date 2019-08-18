30ad11b3688f39106871e780f436447dfde82229
Lionel-D <hello@lionel-d.com>
2019-08-18 09:34:41 +0200

    add basic admin dashboard page

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DashboardController
+ * @package App\Controller\Admin
+ * @author  Lionel DAELEMANS <hello@lionel-d.com>
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin/dashboard", name="admin_dashboard")
+     */
+    public function index()
+    {
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
