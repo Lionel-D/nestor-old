395a9926a8fb9e9d85d79d684412056d7dabaafa
Lionel-D <hello@lionel-d.com>
2019-08-23 16:42:56 +0200

    refactor app styles organisation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-// -- APP WEBPACK LOADER ------------------------------- //
+// -- WEBPACK LOADER ----------------------------------- //
 
 // -- CSS FILES ---------------------------------------- //
 require('../scss/app.scss');
 
 // -- JS FILES ----------------------------------------- //
+// -- Bootstrap dependencies
 //const $ = require('jquery');
 //require('popper.js');
 require('bootstrap');
