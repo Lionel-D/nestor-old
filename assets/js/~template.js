90d72b8704ce2c646a103551be09d9b722cf4f16
Lionel-D <hello@lionel-d.com>
2019-08-23 16:44:04 +0200

    add example template for style organisation

---
+++ assets/js/~template.js
@@ -0,0 +1,10 @@
+// -- WEBPACK LOADER ----------------------------------- //
+
+// -- CSS FILES ---------------------------------------- //
+require('../scss/~template.scss');
+
+// -- JS FILES ----------------------------------------- //
+// -- Bootstrap dependencies
+//const $ = require('jquery');
+//require('popper.js');
+//require('bootstrap');
