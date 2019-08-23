0e9e99961bc22c01617fc67fe498988ccf3de895
Lionel-D <hello@lionel-d.com>
2019-08-23 16:41:13 +0200

    refactor security styles organisation

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,9 +1,10 @@
-// -- SECURITY WEBPACK LOADER -------------------------- //
+// -- WEBPACK LOADER ----------------------------------- //
 
 // -- CSS FILES ---------------------------------------- //
 require('../scss/security.scss');
 
 // -- JS FILES ----------------------------------------- //
+// -- Bootstrap dependencies
 //const $ = require('jquery');
 //require('popper.js');
 //require('bootstrap');
