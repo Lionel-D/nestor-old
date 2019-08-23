77732174b659659f87d821243134501071e09e50
Lionel-D <hello@lionel-d.com>
2019-08-23 16:35:14 +0200

    remove admin specific styling

--- assets/js/admin.js
+++
@@ -1,9 +0,0 @@
-// -- ADMIN WEBPACK LOADER ----------------------------- //
-
-// -- CSS FILES ---------------------------------------- //
-require('../scss/site.scss');
-
-// -- JS FILES ----------------------------------------- //
-//const $ = require('jquery');
-//require('popper.js');
-//require('bootstrap');
